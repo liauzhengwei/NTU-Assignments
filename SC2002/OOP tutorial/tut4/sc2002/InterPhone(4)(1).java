e8fb25494a80d326700341d01dc1f011a925629c
Liau Zheng Wei <liauzhengwei@gmail.com>
2024-05-03 16:31:57 +0800

    UPDATE SC2002

--- SC2002/OOP tutorial/tut4/sc2002/InterPhone(4)(1).java
+++
@@ -1,9 +0,0 @@
-package cecz2002;
-
-public interface InterPhone {
-	
-	public static final int MAX_SCREEN_SIZE = 10;
-	
-	public void ring();
-
-}
---
+++ SC2002/OOP tutorial/tut4/sc2002/InterPhone.java
@@ -0,0 +1,9 @@
+package cecz2002;
+
+public interface InterPhone {
+	
+	public static final int MAX_SCREEN_SIZE = 10;
+	
+	public void ring();
+
+}
